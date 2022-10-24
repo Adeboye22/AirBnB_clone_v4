69ce76bc649e64c825ff769bb2078a2bd7b8e35e
Oluwadamilola Oyeniya <oyeniyaoluwadamilola@outlook.com>
2022-10-24 21:58:26 +0100

    api

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-#!/usr/bin/python3
-"""init file"""
-from flask import Blueprint, render_template, abort
-app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
-=======
 from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
->>>>>>> master
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
