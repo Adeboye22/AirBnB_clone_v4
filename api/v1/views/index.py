4441bb8fee8c112bc7a1ac78458e582224442ba8
Oluwadamilola Oyeniya <oyeniyaoluwadamilola@outlook.com>
2022-10-24 21:59:41 +0100

    api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,39 +1,13 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""following directions"""
-from api.v1.views import app_views
-from flask import jsonify, Blueprint, render_template, abort
-from models import storage
-from models.amenity import Amenity
-=======
 """Index of the application"""
 from api.v1.views import app_views
 from models.amenity import Amenity
 from models.base_model import BaseModel
->>>>>>> master
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-<<<<<<< HEAD
-
-
-@app_views.route('/status', methods=['GET'])
-def status():
-    """status"""
-    return (jsonify({"status": "OK"}))
-
-
-@app_views.route('/stats', methods=['GET'])
-def stats():
-    return (jsonify({"amenities": storage.count(Amenity),
-                     "cities": storage.count(City),
-                     "places": storage.count(Place),
-                     "reviews": storage.count(Review),
-                     "states": storage.count(State),
-                     "users": storage.count(User)}))
-=======
 from models import storage
 from flask import jsonify
 
@@ -60,4 +34,3 @@ def number_objects():
         numb = storage.count(cl)
         dictionary[key] = numb
     return jsonify(dictionary)
->>>>>>> master
